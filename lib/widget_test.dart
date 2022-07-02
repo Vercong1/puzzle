f4ddf42fe4b98c9c7ad45e8dbf631031f159d129
Vercong1 <vercong@gmail.com>
2022-07-02 04:14:48 +0300

    master

--- lib/widget_test.dart
+++ lib/widget_test.dart
@@ -17,7 +17,7 @@ class Main extends StatelessWidget {
 
   @override
 Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size.height;
 
   // final double itemHeight = size.height  * 0.4;
   //   final double itemWidth = size.width;
@@ -25,43 +25,131 @@ Widget build(BuildContext context) {
   // final StatefulHeight = MediaQuery.of(context).padding.top;
 
     return Center(
-      child: Column(
+    child: Column(
     children: <Widget> [
         Expanded(
-            child: Container(color: Colors.white,
-            padding: EdgeInsets.all(15),
+            child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(10),
               // height: heightScreen   * 0.3,
             child: Image.network("https://2peso.ru/wp-content/uploads/2019/09/ris-3-1068x1068.jpg"),
+            
             ),
-            flex:3
+            flex:8
           ),
         Expanded(
           
+    child: Row(
+    crossAxisAlignment: CrossAxisAlignment.center,  mainAxisAlignment: MainAxisAlignment.center ,
+    children:[
+
+        Container(color: Colors.blue, width: 10, height: 10),
+        SizedBox (width: 10,),
+        Container(color: Colors.green,width: 10, height: 10),
+        SizedBox (width: 10,),
+        Container(color: Colors.black,width: 10, height: 10),
+        SizedBox (width: 10,),
+  ],
+),  
+       flex:1
+        ),
+        Expanded(
             child: Container(
-            // height: heightScreen   * 0.3,
-            padding: EdgeInsets.all(10),
+            // height: size   * 2,
+            // padding: EdgeInsets.all(5),
             color: Colors.red,
-            child: GridView(
-            physics: NeverScrollableScrollPhysics(),
+            child: Center(
+              child: GridView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    crossAxisCount: 3,mainAxisSpacing: 10,crossAxisSpacing: 10,   childAspectRatio: (1 / 0.8),
+    crossAxisCount: 3,
+    mainAxisSpacing: 10,
+    crossAxisSpacing: 10,   
+    childAspectRatio: (1 / 0.8),
     
   ),
+  
   children: [
-    
-    Image.network('https://picsum.photos/250?image=1'),
-    Image.network('https://picsum.photos/250?image=2'),
-    Image.network('https://picsum.photos/250?image=3'),
-    Image.network('https://picsum.photos/250?image=3'),
-    Image.network('https://picsum.photos/250?image=3'),
-    Image.network('https://picsum.photos/250?image=3'),
-    Image.network('https://picsum.photos/250?image=3'),
-    Image.network('https://picsum.photos/250?image=3'),
-    Image.network('https://picsum.photos/250?image=3'),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+   Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+    Padding(
+padding: const EdgeInsets.all(10),
+child: Container( color: Colors.blue,
+child: 
+
+  Image.network('https://picsum.photos/250?image=1'), padding: EdgeInsets.all(5),
+    ),
+    ),
+         
   ],
-),        
+        
+),
+            ),        
             ),
-          flex:4           
+          flex:10         
           ),
     ] 
        )
